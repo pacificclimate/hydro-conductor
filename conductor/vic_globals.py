baa6b68009649f040a3bc5ff424d2a8c68ed54dd
James Hiebert <james@hiebert.name>
2015-05-12 10:59:32 -0700

    Refactored get_global_parms to use more concise variables names and to only create lists when necessary

--- conductor/vic_globals.py
+++ conductor/vic_globals.py
@@ -6,6 +6,75 @@ __all__ = ['get_global_parms', 'update_global_parms', 'write_global_parms_file']
 
 from collections import OrderedDict
 
+# James's assessment of proper typing in the global file
+# {'TIME_STEP': int,
+#  'SNOW_STEP': int,
+#  'STARTYEAR': int,
+#  'STARTMONTH': int,
+#  'STARTDAY': int,
+#  'STARTHOUR': int,
+#  'ENDYEAR': int,
+#  'ENDMONTH': int,
+#  'ENDDAY': int,
+#  'FULL_ENERGY': bool,
+#  'FROZEN_SOIL': bool,
+#  'NO_FLUX': bool,
+#  'DIST_PRCP': bool,
+#  'CORRPREC': bool,
+#  'MIN_WIND_SPEED': float,
+#  'PREC_EXPT': float,
+#  'GLACIER_ID': int,
+#  'GLACIER_ACCUM_START_YEAR': int,
+#  'GLACIER_ACCUM_START_MONTH': int,
+#  'GLACIER_ACCUM_START_DAY': int,
+#  'GLACIER_ACCUM_INTERVAL': int,
+#  'OUTPUT_FORCE': bool,
+#  'INIT_STATE': [], #??
+#  'STATENAME': str # filename
+#  'STATEYEAR': int,
+#  'STATEMONTH': int,
+#  'STATEDAY': int,
+#  'STATE_FORMAT': str # NETCDF|??|??,
+#  'GRID_DECIMAL': int #?,
+#  'WIND_H': int # ?,
+#  'MEASURE_H': int # ?,
+#  'ALMA_INPUT': bool,
+#  'FORCING1': str, # filename
+#  'FORCE_FORMAT': str # NETCDF|??|??,
+#  'FORCE_ENDIAN': str # LITTLE|BIG,
+#  'N_TYPES': int,
+#  'FORCE_TYPE': dict,
+#  'FORCE_DT': int,
+#  'FORCEYEAR': int,
+#  'FORCEMONTH': int,
+#  'FORCEDAY': int,
+#  'FORCEHOUR': int,
+#  'NLAYER': int,
+#  'NODES': int,
+#  'SOIL': str, # filename
+#  'BASEFLOW': str,
+#  'ARC_SOIL': bool,
+#  'VEGPARAM': str, # filename
+#  'VEGPARAM_LAI': bool,
+#  'LAI_SRC': str,
+#  'VEGLIB': str, # filename
+#  'ROOT_ZONES': int,
+#  'SNOW_BAND': (int, str ) #filename
+#  'RESULT_DIR': str, # dirname
+#  'OUT_STEP': int,
+#  'SKIPYEAR': int,
+#  'COMPRESS': bool,
+#  'OUTPUT_FORMAT': str, # NETCDF|??
+#  'ALMA_OUTPUT': bool,
+#  'PRT_HEADER': bool,
+#  'PRT_SNOW_BAND': bool,
+#  'NETCDF_ATTRIBUTE': dict,
+#  'N_OUTFILES': int,
+#  'OUTFILE_?': # list of outfiles
+#  'OUTFILE_N': (str (prefix), int)
+#  'OUTVAR_N': list, # of strings
+# }
+
 # To have nested ordered defaultdicts
 class OrderedDefaultdict(OrderedDict):
     # from: http://stackoverflow.com/questions/4126348/how-do-i-rewrite-this-function-to-implement-ordereddict/4127426#4127426
@@ -28,34 +97,41 @@ class OrderedDefaultdict(OrderedDict):
         return self.__class__, args, None, None, iter(self.items())
 
 def get_global_parms(global_parm_file):
-    """ Parses the initial VIC global parameters file created by the user with the settings for the entire VIC-RGM run """
-    global_parms = OrderedDefaultdict()
+    """ Parses the initial VIC global parameters file created by the
+        user with the settings for the entire VIC-RGM run
+    """
+    g = OrderedDefaultdict()
     n_outfile_lines = 0
     with open(global_parm_file, 'r') as f:
         for line in f:
-            #print('line: {}'.format(line))
-            if not (line.isspace() or line.startswith('#')):
-                split_line = line.split()
-                #print('columns: {}'.format(split_line))
-                parm_name = split_line[0]
-                if parm_name == 'OUTFILE': # special case because there are multiple occurrences, not consecutive
-                        n_outfile_lines += 1
-                        parm_name = 'OUTFILE_' + str(n_outfile_lines)
-                elif parm_name == 'OUTVAR': # special case because multiple OUTVAR lines follow each OUTFILE line
-                        parm_name = 'OUTVAR_' + str(n_outfile_lines)
-                try:
-                    if global_parms[parm_name]: # if we've already read one or more entries of this parm_name
-#                        print('parm {} exists already'.format(parm_name))
-                        global_parms[parm_name].append(split_line[1:])
-                except:
-                    global_parms[parm_name] = []
-                    global_parms[parm_name].append(split_line[1:])
-                    #print('global_parms[{}]: {}'.format(parm_name,global_parms[parm_name]))
-                # We need to create a placeholder in this position for INIT_STATE if it doesn't exist in the initial
-                # global parameters file, to be used for all iterations after the first year
-                if parm_name == 'OUTPUT_FORCE': # OUTPUT_FORCE should always immediately precede INIT_STATE in the global file
-                    global_parms['INIT_STATE'] = []
-    return global_parms
+            if line.isspace() or line.startswith('#'):
+                continue
+
+            name, value = line.split(None, 1)
+            value = value.strip()
+
+            # special case because there are multiple occurrences, not consecutiev
+            if name == 'OUTFILE':
+                n_outfile_lines += 1
+                name = 'OUTFILE_' + str(n_outfile_lines)
+            # special case because multiple OUTVAR lines follow each OUTFILE line
+            elif name == 'OUTVAR':
+                name = 'OUTVAR_' + str(n_outfile_lines)
+
+            if name in g:
+                # print('parm {} exists already'.format(name))
+                if type(g[name]) != list:
+                    g[name] = [ g[name] ]
+                g[name].append(value)
+            else:
+                g[name] = value
+
+            # We need to create a placeholder in this position for INIT_STATE if it doesn't exist in the initial
+            # global parameters file, to be used for all iterations after the first year
+            if name == 'OUTPUT_FORCE': # OUTPUT_FORCE should always immediately precede INIT_STATE in the global file
+                g['INIT_STATE'] = []
+
+    return g
 
 def update_global_parms(global_parms, temp_vpf, temp_snb, num_snow_bands, start_date, end_date, init_state_file, state_date):
     """ Updates the global_parms dict at the beginning of an annual iteration """
