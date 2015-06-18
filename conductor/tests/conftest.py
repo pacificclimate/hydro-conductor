3cf39014000f82adea84d1c3dfc276ea58315937
Michael Fischer <mfischer@uvic.ca>
2015-06-17 18:09:39 -0700

    Fleshes out 64 pixel toy test domain to include spatial DEM to support testing of update_area_fracs().

--- conductor/tests/conftest.py
+++ conductor/tests/conftest.py
@@ -50,6 +50,7 @@
 
 import io
 from pkg_resources import resource_stream, resource_filename
+import numpy as np
 
 import pytest
 
@@ -81,9 +82,9 @@ def sample_global_file_string():
 def simple_unit_test_parms():
 
     # initial median band elevations
-    test_median_elevs_simple = [2035, 2172, 2241, 2315]
-    test_median_elevs = {'12345': [2035, 2172, 2241, 2315],
-                        '23456': [1921, 2045, 2164]} 
+    test_median_elevs_simple = [2040, 2120, 2250, 2330]
+    test_median_elevs = {'12345': [2040, 2120, 2250, 2330],
+                        '23456': [1970, 2005, 2120]} 
 
     # Just for single cell unit tests:
     test_area_fracs_simple = [0.1875, 0.25, # Band 0 (11, 19)
@@ -151,17 +152,19 @@ def toy_domain_64px_cells():
                         '19': [0.1, 1.0, 0.1, 0.0, 0.1, 0.0], # 19
                         '22': [0.1, 1.0, 0.1, 0.0, 0.1, 0.0]} # 22
 
-    # Spatial DEM layout                    
-    initial_dem_cell_12345 = np.array([ [2065, 2055, 2045, 2035, 2025, 2015, 2005, 2000],
+    # Spatial DEM layout
+    initial_dem_by_cells = { cell_ids[0]:                  
+                                np.array([[2065, 2055, 2045, 2035, 2025, 2015, 2005, 2000],
                                         [2075, 2100, 2120, 2140, 2130, 2120, 2100, 2005],
                                         [2085, 2110, 2250, 2270, 2260, 2240, 2110, 2010],
                                         [2090, 2120, 2260, 2377, 2310, 2245, 2125, 2015],
                                         [2070, 2110, 2250, 2340, 2320, 2250, 2130, 2020],
                                         [2090, 2105, 2200, 2210, 2220, 2220, 2120, 2015],
                                         [2090, 2100, 2105, 2110, 2140, 2150, 2130, 2010],
-                                        [2080, 2075, 2065, 2055, 2045, 2035, 2020, 2000] ])
+                                        [2080, 2075, 2065, 2055, 2045, 2035, 2020, 2000] ]),
 
-    initial_dem_cell_23456 = np.array([ [1970, 1975, 1995, 1995, 1975, 1965, 1960, 1960],
+                            cell_ids[1]:
+                                np.array([[1970, 1975, 1995, 1995, 1975, 1965, 1960, 1960],
                                         [1970, 2000, 2045, 2055, 2005, 2005, 2000, 1965],
                                         [1975, 2000, 2100, 2155, 2160, 2140, 2000, 1970],
                                         [1985, 2005, 2105, 2160, 2180, 2130, 2000, 1975],
@@ -169,28 +172,119 @@ def toy_domain_64px_cells():
                                         [1980, 2005, 2105, 2105, 2110, 2100, 2000, 1980],
                                         [1970, 2000, 2000, 2020, 2035, 2025, 2000, 1970],
                                         [1965, 1965, 1970, 1970, 1975, 1960, 1950, 1960] ])
+                    }
 
-    initial_dem = np.concatenate((initial_dem_cell_12345, initial_dem_cell_23456), axis=1)
-
-    initial_glacier_mask_cell_12345 = np.array( [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+    initial_glacier_mask_by_cells = { cell_ids[0]: 
+                                        np.array([[ 0, 0, 0, 0, 0, 0, 0, 0 ],
                                                 [ 0, 1, 1, 1, 1, 1, 0, 0 ],
                                                 [ 0, 1, 1, 1, 1, 1, 0, 0 ],
                                                 [ 0, 1, 1, 0, 0, 1, 0, 0 ],
                                                 [ 0, 1, 1, 0, 0, 1, 0, 0 ],
+                                                [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+                                                [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+                                                [ 0, 0, 0, 0, 0, 0, 0, 0 ]]),
+                                    cell_ids[1]:
+                                        np.array([[ 0, 0, 1, 1, 0, 0, 0, 0 ],
+                                                [ 0, 0, 1, 1, 0, 0, 0, 0 ],
+                                                [ 0, 0, 0, 1, 1, 1, 0, 0 ],
+                                                [ 0, 0, 0, 1, 1, 1, 0, 0 ],
+                                                [ 0, 0, 0, 1, 1, 0, 0, 0 ],
                                                 [ 0, 0, 0, 0, 0, 0, 0, 0 ],
                                                 [ 0, 0, 0, 0, 0, 0, 0, 0 ],
                                                 [ 0, 0, 0, 0, 0, 0, 0, 0 ]])
+                                    }
 
-#TODO: fill in this glacier mask
-    initial_glacier_mask_cell_23456 = np.array( [ 0, 0, 0, 0, 0, 0, 0, 0 ],
-                                                [ 0, 1, 1, 1, 1, 1, 0, 0 ],
-                                                [ 0, 1, 1, 1, 1, 1, 0, 0 ],
-                                                [ 0, 1, 1, 0, 0, 1, 0, 0 ],
-                                                [ 0, 1, 1, 0, 0, 1, 0, 0 ],
-                                                [ 0, 0, 0, 0, 0, 0, 0, 0 ],
-                                                [ 0, 0, 0, 0, 0, 0, 0, 0 ],
-                                                [ 0, 0, 0, 0, 0, 0, 0, 0 ]])
+    def build_padded_dem_aligned_map(array_1, array_2, padding_thickness):
+        ''' Helper function to create a map that is aligned with a DEM made up of two adjacent
+            rectangular pixel arrays (representing 2 VIC cells), with NaN padding around
+            the edges (simulating the output from get_rgm_pixel_mapping(), where some
+            pixels that are read in do not belong to either VIC cell). This can be used to 
+            generate test instances of surf_dem, bed_dem, cellid_map, glacier_mask
+        '''
+        vertical_pad = np.empty((padding_thickness, 2*padding_thickness+len(array_1[0])+len(array_2[0])))
+        vertical_pad.fill(np.nan)
+        horizontal_pad = np.empty((len(array_1), padding_thickness))
+        horizontal_pad.fill(np.nan)
+        padded_map = np.concatenate((array_1, array_2), axis=1)
+        padded_map = np.concatenate((horizontal_pad, padded_map), axis=1)
+        padded_map = np.concatenate((padded_map, horizontal_pad), axis=1)
+        padded_map = np.concatenate((vertical_pad, padded_map), axis=0)
+        padded_map = np.concatenate((padded_map, vertical_pad), axis=0)
+        return padded_map
 
+    # Create cellid_map that is padded by 2 rows & columns of np.NaNs (to simulate mapping
+    # of valid pixels to VIC cells, as read in by get_rgm_pixel_mapping())
+    cellid_map_cell_0 = np.empty((8,8))
+    cellid_map_cell_0.fill(cell_ids[0])
+    cellid_map_cell_1 = np.empty((8,8))
+    cellid_map_cell_1.fill(cell_ids[1])
+    cellid_map = build_padded_dem_aligned_map(cellid_map_cell_0, cellid_map_cell_1, 2)
 
-    return cells, cell_ids, num_snow_bands, band_size, expected_band_ids, expected_root_zone_parms
+    # Create initial surf_dem with padding of 2
+    surf_dem = build_padded_dem_aligned_map(initial_dem_by_cells[cell_ids[0]], initial_dem_by_cells[cell_ids[1]], 2)
+    # Non-padded version of surf_dem:
+    #surf_dem = np.concatenate((initial_dem_by_cells[cell_ids[0]], initial_dem_by_cells[cell_ids[1]]), axis=1)
+
+    # Create initial glacier mask with padding of 2
+    glacier_mask = build_padded_dem_aligned_map(initial_glacier_mask_by_cells[cell_ids[0]], initial_glacier_mask_by_cells[cell_ids[1]], 2)
+
+    # The toy DEM above, broken down by elevation bands.  Useful for checking median elevations
+    cell_band_pixel_elevations = {
+        cell_ids[0]: [
+                    # band 0, median: 2040
+                    [2065, 2055, 2045, 2035, 2025, 2015, 2005, 2000,
+                     2075,                                     2005,
+                     2085,                                     2010,
+                     2090,                                     2015,
+                     2070,                                     2020,
+                     2090,                                     2015,
+                     2090,                                     2010,
+                     2080, 2075, 2065, 2055, 2045, 2035, 2020, 2000],
+
+                    # band 1, median: 2120
+                    [2100, 2120, 2140, 2130, 2120, 2100,
+                     2110,                         2110,
+                     2120,                         2125,
+                     2110,                         2130,
+                     2105,                         2120,
+                     2100, 2105, 2110, 2140, 2150, 2130],
+
+                    # band 2, median: 2250
+                    [2250, 2270, 2260, 2240,
+                     2260,             2250,
+                     2250,             2250,
+                     2200, 2210, 2220, 2220],
+
+                    # band 3, median: 2330
+                    [2377, 2310,
+                    2340, 2320]
+                    ],
+        cell_ids[1]: [
+                    # band 0, median: 1970
+                    [1970, 1975, 1995, 1995, 1975, 1965, 1960, 1960,
+                    1970,                                       1965,
+                    1975,                                       1970,
+                    1985,                                       1975,
+                    1990,                                       1980,
+                    1980,                                       1980,
+                    1970,                                       1970,
+                    1965, 1965, 1970, 1970, 1975, 1960, 1950, 1960],
+
+                    # band 1, median: 2005
+                    [2000, 2045, 2055, 2005, 2005, 2000,
+                    2000,                           2000,
+                    2005,                           2000,
+                    2010,                           2005,
+                    2005,                           2000,
+                    2000, 2000, 2020, 2035, 2025, 2000],
+
+                    # band 2, median: 2120
+                    [2100, 2155, 2160, 2140,
+                    2105, 2160, 2180, 2130,
+                    2110, 2150, 2140, 2105,
+                    2105, 2105, 2110, 2100],
+                    ] }
+
+    return cells, cell_ids, num_snow_bands, band_size, expected_band_ids, expected_root_zone_parms, \
+            cellid_map, surf_dem, glacier_mask, cell_band_pixel_elevations
 
